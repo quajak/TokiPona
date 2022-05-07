ed1b2dfffa09994457a4ee180cdef7c268c0641c
Quajak <jasper.gerigk@gmx.net>
2022-05-07 18:29:21 +0200

    Improve main page

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,8 +4,10 @@ import { ref } from 'vue'
 </script>
 
 <template>
-  <h1>Toki!</h1>
-  <p>This site can be used to practise learning the conlanguage Toki Pona. Simply register an account to start practising!</p>
+  <h1 class="title is-5">Toki!</h1>
+  <p>I made this website to help me learn the conlanguage Toki Pona. I hope it can help you as well! Simply register an account to start practising!</p>
+  <p style="margin-top:20px">All credit goes to Sonja Lang for creating the language and to Jan Lope for his <a href="https://jan-lope.github.io/Toki_Pona_lessons_English/">resources</a>, espeically the dictionary.</p>
+  <p style="margin-top:5px">The code for this website is open source and can be found on <a href="https://github.com/quajak/TokiPona">github</a>. Any feedback is appreciated!</p>
 </template>
 
 <style scoped lang="scss">
