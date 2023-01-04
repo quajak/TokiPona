888543f161ad613ad9dd9ab0345ee2cbd0ce3e1d
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-01-04 06:26:06 +0100

    Use new server

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.xsrfCookieName = "csrf_access_token"
 axios.defaults.xsrfHeaderName = "X-CSRF-TOKEN"
 
-export const basePath = localStorage.getItem("basePath") || "https://kama-sona.dilab.co"
+export const basePath = localStorage.getItem("basePath") || "https://kama-sona.justso.de"
 
 const authService = axios.create({
     baseURL: basePath + "/api/auth",
